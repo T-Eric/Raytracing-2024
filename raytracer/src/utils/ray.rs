1f741f74f3876259beada03c48980183995bfcc8
T-Eric <1666384703@qq.com>
2024-06-29 17:38:21 +0800

    for pic 1.2

--- raytracer/src/utils/ray.rs
+++ raytracer/src/utils/ray.rs
@@ -1,30 +1,30 @@
 use crate::utils::vec3::{Point3, Vec3};
 pub struct Ray {
-    orig: Point3,
+    _orig: Point3,
     dir: Vec3,
 }
 
 impl Ray {
     pub fn new(origin: &Point3, direction: &Vec3) -> Self {
         Ray {
-            orig: origin.clone(),
+            _orig: origin.clone(),
             dir: direction.clone(),
         }
     }
     pub fn _zero() -> Self {
         Ray {
-            orig: Point3::zero(),
+            _orig: Point3::zero(),
             dir: Vec3::zero(),
         }
     }
     pub fn _copy(other: &Ray) -> Self {
         Ray {
-            orig: other.orig.clone(),
+            _orig: other._orig.clone(),
             dir: other.dir.clone(),
         }
     }
     pub fn _origin(&self) -> &Point3 {
-        &self.orig
+        &self._orig
     }
     pub fn direction(&self) -> &Vec3 {
         &self.dir
@@ -32,6 +32,6 @@ impl Ray {
 
     // at time t, where is it (dir is the speed)
     pub fn _at(&self, t: f64) -> Point3 {
-        self.orig.clone() + (self.dir.clone() * t)
+        self._orig.clone() + (self.dir.clone() * t)
     }
 }
