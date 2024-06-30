78173d8f737cec99eae379b7cf239e31925f9364
T-Eric <1666384703@qq.com>
2024-06-30 19:03:17 +0800

    for pic 1.14

--- raytracer/src/utils/material.rs
+++ raytracer/src/utils/material.rs
@@ -1,7 +1,7 @@
 use crate::utils::color::Color;
 use crate::utils::hittable::HitRecord;
 use crate::utils::ray::Ray;
-use crate::utils::vec3::{random_unit_vector, reflect};
+use crate::utils::vec3::{dot, random_unit_vector, reflect, unit_vector};
 
 pub trait Material {
     fn scatter(
@@ -21,6 +21,7 @@ pub struct Lambertian {
 // Metal, full reflection
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Lambertian {
@@ -53,9 +54,10 @@ impl Material for Lambertian {
 }
 
 impl Metal {
-    pub fn new(albedo: &Color) -> Self {
+    pub fn new(albedo: &Color, fuzz: f64) -> Self {
         Metal {
             albedo: albedo.clone(),
+            fuzz: if fuzz < 1.0 { fuzz } else { 1.0 },
         }
     }
 }
@@ -66,11 +68,14 @@ impl Material for Metal {
         r_in: &Ray,
         rec: &HitRecord,
         _attenuation: &mut Color,
-        _scattered: &mut Ray,
+        scattered: &mut Ray,
     ) -> bool {
-        let reflected = reflect(r_in.direction(), &rec.normal);
-        *_scattered = Ray::new(&rec.p, &reflected);
+        let mut reflected = reflect(r_in.direction(), &rec.normal);
+        // fuzz operation
+        reflected = unit_vector(&reflected) + (random_unit_vector() * self.fuzz);
+        *scattered = Ray::new(&rec.p, &reflected);
         *_attenuation = self.albedo.clone();
-        true
+
+        dot(scattered.direction(), &rec.normal) > 0.0
     }
 }
