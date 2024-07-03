8eb66d2f75890b4f067f2271267b96eaa5c529a9
T-Eric <1666384703@qq.com>
2024-07-04 00:04:51 +0800

    for pic 2.23(low quality)

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -601,7 +601,7 @@ fn cornell_smoke() -> std::io::Result<()> {
     cam.focus_dist = 10.0;
 
     let savepath = String::from("output/book2");
-    let savefile = savepath.clone() + &*String::from("/22.png");
+    let savefile = savepath.clone() + &*String::from("/0.png");
     let path = Path::new(&savepath);
 
     if !path.exists() {
@@ -616,8 +616,154 @@ fn cornell_smoke() -> std::io::Result<()> {
     eprintln!("duration:{:?}ms", now);
     Ok(())
 }
+
+fn final_scene(
+    image_width: i32,
+    samples_per_pixel: i32,
+    max_recurse_depth: i32,
+) -> std::io::Result<()> {
+    let now = Instant::now();
+
+    let mut boxes1 = HittableList::default();
+    let mut rng = rand::thread_rng();
+
+    let ground = Arc::new(Lambertian::new_color(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rng.gen_range(1.0..101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(cube(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                ground.clone(),
+            ));
+        }
+    }
+
+    let mut world = HittableList::default();
+
+    world.add(Arc::new(BvhNode::new_list(&mut boxes1)));
+
+    let light = Arc::new(DiffuseLight::new_color(Color::new(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(
+        Point3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        light,
+    )));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_mat = Arc::new(Lambertian::new_color(Color::new(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::new_motive(
+        center1, center2, 50.0, sphere_mat,
+    )));
+
+    world.add(Arc::new(Sphere::new_static(
+        Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Arc::new(Sphere::new_static(
+        Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+
+    let mut boundary = Arc::new(Sphere::new_static(
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstMedium::new_color(
+        boundary,
+        0.2,
+        Color::new(0.2, 0.4, 0.9),
+    )));
+    boundary = Arc::new(Sphere::new_static(
+        Point3::default(),
+        5000.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Arc::new(ConstMedium::new_color(
+        boundary,
+        0.0001,
+        Color::new(1.0, 1.0, 1.0),
+    )));
+
+    let emat = Arc::new(Lambertian::new_arc(Arc::new(ImageTexture::new(
+        "source/earthmap.jpg",
+    ))));
+    world.add(Arc::new(Sphere::new_static(
+        Point3::new(400.0, 200.0, 400.0),
+        100.0,
+        emat,
+    )));
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.add(Arc::new(Sphere::new_static(
+        Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::new_arc(pertext)),
+    )));
+
+    let mut boxes2 = HittableList::default();
+    let white = Arc::new(Lambertian::new_color(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _ in 0..ns {
+        boxes2.add(Arc::new(Sphere::new_static(
+            Point3::random_in(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+    world.add(Arc::new(Translate::new(
+        Arc::new(RotateY::new(Arc::new(BvhNode::new_list(&mut boxes2)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0),
+    )));
+
+    let mut cam = Camera::default();
+
+    cam.aspect_ratio = 1.0;
+    cam.image_width = image_width;
+    cam.samples_per_pixel = samples_per_pixel;
+    cam.max_recurse_depth = max_recurse_depth;
+
+    cam.vfov = 40.0;
+    cam.lookfrom = Point3::new(478.0, 278.0, -600.0);
+    cam.lookat = Point3::new(278.0, 278.0, 0.0);
+    cam.vup = Vec3::new(0.0, 1.0, 0.0);
+
+    cam.defocus_angle = 0.0;
+
+    let savepath = String::from("output/book2");
+    let savefile = savepath.clone() + &*String::from("/23.png");
+    let path = Path::new(&savepath);
+
+    if !path.exists() {
+        fs::create_dir_all(path)?;
+        cam.render(world, savefile);
+    } else {
+        cam.render(world, savefile);
+    }
+
+    let now = now.elapsed().as_millis();
+    eprintln!();
+    eprintln!("duration:{:?}ms", now);
+    Ok(())
+}
+
 fn main() {
-    match 8 {
+    match 10 {
         1 => {
             bouncing_spheres().expect("Fail!");
         }
@@ -641,6 +787,10 @@ fn main() {
         }
         8 => {
             cornell_smoke().expect("Fail!");
+        }
+        9 => final_scene(800, 10000, 40).expect("Fail!"),
+        10 => {
+            final_scene(300, 100, 4).expect("Fail!");
         }
         _ => (),
     }
